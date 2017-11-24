59a880546e5c6a196f470876cb5aaa29eb610013
Himanshu Mehta <himanshum2497@gmail.com>
2017-11-24 00:09:04 -0800

    working client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div style={styles}>
+          <Nav/>
+          {this.props.children}
+          <Footer/>
+        </div>
     );
   }
 }
 
+const styles = {
+  background:'white'
+}
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css'
+import {Route, BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './components/Home'
+import About from './components/About'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <BrowserRouter>
+      <App>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </App>
+    </BrowserRouter>
+  </MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();

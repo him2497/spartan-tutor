addb15a94730e00b1659c66d24dfede94d8fdd82
Himanshu Mehta <himanshum2497@gmail.com>
2019-03-13 05:11:48 -0700

    removed the padding from landing page

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '683777212784-4e0pk9ameff1ulabpjkdis9an5a97ls6.apps.googleusercontent.com',
+  googleClientSecret: 'EWS7t8sjBRjnilowLU-g2eCC',
+  mongoURI: 'mongodb://him2497:himanshu24@ds145800.mlab.com:45800/spartan-tutor-dev',
+  cookieKey: 'nsnjkflnjvnkfnvksd343409nkfjvnsjknjkvfddnj430jannrfjkbnks0934rrb3jh;ankndjne023jonslsmong',
+  stripePublishableKey: 'pk_test_DQ8pRAyPSWieMomDowZlvfn9',
+  stripeSecretKey: 'sk_test_vgGCgRwj4kZZVtuASle8izPo',
+};
